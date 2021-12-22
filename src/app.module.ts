c88086b5e0e3a99176deccd53cfc8aa16ca7d4d0
Ykoh <ykoh@student.42seoul.kr>
2021-12-22 18:39:35 +0900

    Feat: register axios interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module, OnModuleInit } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,7 +9,33 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule implements OnModuleInit {
+  constructor(private readonly httpService: HttpService) {}
+
   onModuleInit() {
-    // register interceptors in here
+    // 요청 인터셉터 추가하기
+    this.httpService.axiosRef.interceptors.request.use(
+      (config) => {
+        // 요청이 전달되기 전에 작업 수행
+        return config;
+      },
+      (error) => {
+        // 요청 오류가 있는 작업 수행
+        return Promise.reject(error);
+      },
+    );
+
+    // 응답 인터셉터 추가하기
+    this.httpService.axiosRef.interceptors.response.use(
+      (response) => {
+        // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+        // 응답 데이터가 있는 작업 수행
+        return response;
+      },
+      (error) => {
+        // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+        // 응답 오류가 있는 작업 수행
+        return Promise.reject(error);
+      },
+    );
   }
 }
