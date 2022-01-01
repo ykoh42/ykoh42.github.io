ec7af3b2dc47eba326bc302d2ac1c525c33822f7
Ykoh <ykoh@student.42seoul.kr>
2022-01-01 19:53:49 +0900

    feat(api): login, profile, logout API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,26 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('login')
+  async logIn(@Req() req) {
+    req.session.user = { id: 20 };
+    await req.session.save();
+    return 'ok';
+  }
+
+  @Get('profile')
+  profile(@Req() req) {
+    if (req.session.user === undefined) {
+      return 'user undefined';
+    }
+    return req.session.user;
+  }
+
+  @Post('logout') // use post to prevent cached
+  async logOut(@Req() req) {
+    req.session.destroy();
+    return 'logout ok';
   }
 }
