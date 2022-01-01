c9a4e0936b0f18ccd737ab8dcf2e04867929c399
Ykoh <ykoh@student.42seoul.kr>
2022-01-01 19:33:58 +0900

    feat(iron-session): iron-session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ironSession } from 'iron-session/express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const session = ironSession({
+    cookieName: 'iron-session/examples/express',
+    password: process.env.SECRET_COOKIE_PASSWORD,
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  });
+  app.use(session);
+
   await app.listen(3000);
 }
 bootstrap();
