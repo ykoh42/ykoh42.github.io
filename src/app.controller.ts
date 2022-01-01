c7d4e19d5f45aac34bca0e820299fd7dfc3f5336
Ykoh <ykoh@student.42seoul.kr>
2022-01-01 20:13:19 +0900

    chore(remove): remove unused files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,7 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('login')
   async logIn(@Req() req) {
     req.session.user = { id: 20 };
